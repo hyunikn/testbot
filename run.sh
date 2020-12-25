4f4969a76e6dfd68c92a7260f7d7e25b92e4e816
hyunik.na <hyunik.na@atto-research.com>
2020-12-25 17:26:23 +0900

    minor: pom.xml

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n -ea -d64 -cp target/test-driver-java.jar \
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n -ea -d64 -cp target/testbot-java.jar \
                     io.github.hyunikn.testbot.Main $@
